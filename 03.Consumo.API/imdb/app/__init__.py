07370e35d1db91835798ee7b51db68c87e07e8dd
Abdel G. Martínez L <abdel.g.martinez.l@gmail.com>
2017-10-02 20:23:40 -0500

    Consumo de API

---
+++ 03.Consumo.API/imdb/app/__init__.py
@@ -0,0 +1,21 @@
+from tweepy import Stream, OAuthHandler
+from tweepy.streaming import StreamListener
+
+cke = "S4IYPnnEScDG2USwBVke3MCdZ"
+cse = "FVJbpmKAhFO8jVWiOoRl1gt5tDCeyy0RTkZheAmHT7lVm7tfj2"
+ato = "137520613-tyW9Ecq8P2Q8mmk2OFS41re1nDKbkDNn8JCJT4R9"
+ase = "MZPl8APzi746wnob4lP0FZefFnOB0mtcH0sAHOWZKHbB5"
+
+auth = OAuthHandler(cke, cse)
+auth.set_access_token(ato, ase)
+
+#class TLListener(StreamListener):
+#    def on_data(self, raw_data):
+#        print(raw_data)
+#        return(True)
+
+#    def on_error(self, status_code):
+#        print(status_code)
+
+#twStream = Stream(auth, TLListener())
+#twStream.filter(track=["uip"])
