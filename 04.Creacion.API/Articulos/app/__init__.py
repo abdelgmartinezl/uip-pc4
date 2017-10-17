9db3415d00d2054cad162978ba93187542a9bd7b
Abdel G. Martínez L <abdel.g.martinez.l@gmail.com>
2017-10-16 20:35:47 -0500

    Cambios al ejemplo

--- 04.Creacion.API/Articulos/app/__init__.py
+++ 04.Creacion.API/Articulos/app/__init__.py
@@ -1,8 +1,18 @@
 from flask import Flask
 from flask_restful import Api, Resource
 from flask_restful import reqparse
+from flask.ext.mysql import MySQL
 
+mysql = MySQL()
 app = Flask(__name__)
+
+app.config['MYSQL_DATABASE_USER'] = 'uip'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'uip'
+app.config['MYSQL_DATABASE_DB'] = 'articulos'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+
+mysql.init_app(app)
+
 api = Api(app)
 
 class CrearUsuario(Resource):
@@ -15,7 +25,17 @@ class CrearUsuario(Resource):
             _correo = args['correo']
             _passwd = args['passwd']
 
-            return {'correo': args['correo'], 'password': args['passwd']}
+            conn = mysql.connect()
+            cursor = conn.cursor()
+            cursor.callproc('spCrearUsuario', (_correo,_passwd))
+            resultado = cursor.fetchall()
+
+            if len(resultado) is 0:
+                conn.commit()
+                return {'estado': '200', 'mensaje': 'Usuario creado exitosamente'}
+            else:
+                return {'estado': '1234', 'mensaje': str(resultado[0])}
+
         except Exception as e:
             return {'error': str(e)}
 
