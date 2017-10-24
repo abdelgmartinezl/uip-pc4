2623de309a230759aff51041a8ab1c3c6092acf9
Abdel G. Martínez L <abdel.g.martinez.l@gmail.com>
2017-10-23 20:31:15 -0500

    Programación con hilos OOP

---
+++ 05.Hilos/Ejemplo/app/hilo2.py
@@ -0,0 +1,28 @@
+import threading
+import logging
+
+logging.basicConfig(level=logging.DEBUG,
+                    filename='hilo2.log',
+                    format='%(asctime)s - %(threadName)s - %(levelname)s - %(message)s',
+                    filemode='w')
+
+class MiHilo(threading.Thread):
+    def __init__(self, numero):
+        threading.Thread.__init__(self)
+        self.numero = numero
+
+    def run(self):
+        logging.debug('Llamando a la operacion')
+        operacion(self.numero)
+
+def operacion(x):
+    logging.debug('ejecucion de la operacion')
+    r = x * 2
+    logging.debug('la operacion da como resultado: {}'.format(str(r)))
+
+if __name__ == '__main__':
+    lista_hilos = ['Juan', 'Petra', 'Josefa', 'Xenobia']
+    for i in range(4):
+        mi_hilo = MiHilo(i)
+        mi_hilo.setName(lista_hilos[i])
+        mi_hilo.start()
