08cdec961a4beb887d13526f70fa7cb3ffafdb69
Abdel Gadiel Martinez Lassonde <abdel.g.martinez.l@gmail.com>
2017-10-30 19:22:16 -0500

    Tema nuevo

--- 06.Coroutines/Ejemplos/app/__init__.py
+++ 06.Coroutines/Ejemplos/app/__init__.py
@@ -2,6 +2,19 @@ def hola_mundo():
     yield "Hola"
     yield "Mundo"
 
-generador = hola_mundo()
-print(next(generador))
-print(next(generador))
+def generar_secuencia():
+    numero = 0
+    while True:
+        yield numero
+        numero = numero + 1
+
+if __name__ == "__main__":
+    generador = hola_mundo()
+    print(next(generador))
+    print(next(generador))
+
+    numeros = generar_secuencia()
+    for n in numeros:
+        print(n)
+        if n > 14:
+            break
