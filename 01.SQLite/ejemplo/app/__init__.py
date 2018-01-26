6bfc37b9b40daf68b7b05a82c9fef5f41ed2289a
Abdel G. Martínez L <abdel.g.martinez.l@gmail.com>
2018-01-26 18:05:02 -0500

    Ejemplo de SQLite

--- 01.SQLite/ejemplo/app/__init__.py
+++ 01.SQLite/ejemplo/app/__init__.py
@@ -7,11 +7,11 @@ def cifrar_password(password):
     cifrado = hashlib.sha512(password.encode('utf-8')).hexdigest()
     return cifrado
 
-db = sqlite3.connect('data/prueba') # Base de datos en un archivo
+db = sqlite3.connect('prueba') # Base de datos en un archivo
 db.create_function('cifrar', 1, cifrar_password)
 cursor = db.cursor()
-cursor.execute('''CREATE TABLE clave(id INTEGER PRIMARY KEY,
-                  email TEXT, password TEXT)''')
+#cursor.execute('''CREATE TABLE clave(id INTEGER PRIMARY KEY,
+#                  email TEXT, password TEXT)''')
 usuario = input("Correo: ")
 password = input("Password: ")
 cursor.execute('''INSERT INTO clave(email, password) VALUES 
@@ -23,46 +23,46 @@ db.commit()
 #for fila in resultado:
 #    print("{0} : {1}".format(fila[0], fila[1]))
 
-#id = input("ID de usuario a eliminar: ")
-#for fila in resultado:
-#    if int(id) == int(fila[0]):
-#        cursor.execute('''DELETE FROM usuario WHERE id = ?''',
-#                       (id,))
-#        db.commit()
+# id = input("ID de usuario a eliminar: ")
+# for fila in resultado:
+#     if int(id) == int(fila[0]):
+#         cursor.execute('''DELETE FROM usuario WHERE id = ?''',
+#                        (id,))
+#         db.commit()
 
-#id = input("ID de usuario a modificar: ")
-#for fila in resultado:
-#    if int(id) == int(fila[0]):
-#        nuevo_nombre = input("Nombre Nuevo: ")
-#        cursor.execute('''UPDATE usuario SET nombre = ? WHERE
-#                          id = ?''', (nuevo_nombre, id))
-#        db.commit()
+# id = input("ID de usuario a modificar: ")
+# for fila in resultado:
+#     if int(id) == int(fila[0]):
+#         nuevo_nombre = input("Nombre Nuevo: ")
+#         cursor.execute('''UPDATE usuario SET nombre = ? WHERE
+#                           id = ?''', (nuevo_nombre, id))
+#         db.commit()
 
-#cursor.execute('''CREATE TABLE usuario(id INTEGER PRIMARY KEY, nombre TEXT,
-#                    telefono TEXT, email TEXT UNIQUE, password TEXT)''')
-#db.commit()
-#cursor.execute('''DROP TABLE usuario''')
-#db.commit()
+# cursor.execute('''CREATE TABLE usuario(id INTEGER PRIMARY KEY, nombre TEXT,
+#                     telefono TEXT, email TEXT UNIQUE, password TEXT)''')
+# db.commit()
+# cursor.execute('''DROP TABLE usuario''')
+# db.commit()
 
-#nombre = input("Nombre: ")
-#telefono = input("Telefono: ")
-#email = input("Email: ")
-#password = input("Password: ")
+# nombre = input("Nombre: ")
+# telefono = input("Telefono: ")
+# email = input("Email: ")
+# password = input("Password: ")
 
-#cursor.execute('''INSERT INTO usuario(nombre, telefono, email, password)
-#                  VALUES(?,?,?,?)''', (nombre, telefono, email, password))
-#cursor.execute('''INSERT INTO usuario(nombre, telefono, email,
-#               password) VALUES(:nombre, :telefono, :email,
-#                :password)''',
-#                {'nombre':nombre, 'telefono':telefono,
-#                'email':email, 'password':password})
-#db.commit()
+# cursor.execute('''INSERT INTO usuario(nombre, telefono, email, password)
+#                   VALUES(?,?,?,?)''', (nombre, telefono, email, password))
+# cursor.execute('''INSERT INTO usuario(nombre, telefono, email,
+#                password) VALUES(:nombre, :telefono, :email,
+#                 :password)''',
+#                 {'nombre':nombre, 'telefono':telefono,
+#                 'email':email, 'password':password})
+# db.commit()
 
-#usuarios = [("Juan","2323222","juan2@juan.org","12345"),
-#            ("Maria","4352122","maria2@maria.com","maria123"),
-#            ("Pedro","7773345","pedro2@juan.org","p3dr0")]
-#cursor.executemany('''INSERT INTO usuario(nombre, telefono, email,
-#                    password) VALUES (?,?,?,?)''', usuarios)
-#db.commit()
+# usuarios = [("Juan","2323222","juan2@juan.org","12345"),
+#             ("Maria","4352122","maria2@maria.com","maria123"),
+#             ("Pedro","7773345","pedro2@juan.org","p3dr0")]
+# cursor.executemany('''INSERT INTO usuario(nombre, telefono, email,
+#                     password) VALUES (?,?,?,?)''', usuarios)
+# db.commit()
 
-db.close()
+# db.close()
