85d9e350069466873d8f9311b8bf44458a203202
Abdel G. Martínez L <abdel.g.martinez.l@gmail.com>
2018-02-26 21:14:25 -0500

    vamos pa lante

--- 09.Flask/ejemplo/__init__.py
+++ 09.Flask/ejemplo/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, url_for, request
+from flask import Flask, redirect, url_for, request, render_template
 app = Flask(__name__)
 
 # @app.route('/')
@@ -37,24 +37,36 @@ app = Flask(__name__)
 #         return redirect(url_for('saludar_invitado', 
 #             invitado=nombre))
 
-@app.route('/vive')
-def vivir():
-    return 'Puedes vivir en Brazil'
+# @app.route('/vive')
+# def vivir():
+#     return 'Puedes vivir en Brazil'
 
-@app.route('/muere')
-def morir():
-    return 'Cuidado con irte a Brazil'
+# @app.route('/muere')
+# def morir():
+#     return 'Cuidado con irte a Brazil'
 
-@app.route('/brasil', methods=['POST', 'GET'])
-def brasil():
-    if request.method == 'POST':
-        salario = float(request.form['sm'])
-        if salario > 3750.0:
-            return redirect(url_for('vivir'))
-        else:
-            return redirect(url_for('morir'))
-    else:
-        return 'Para que quieres ir?'
+# @app.route('/brasil', methods=['POST', 'GET'])
+# def brasil():
+#     if request.method == 'POST':
+#         salario = float(request.form['sm'])
+#         if salario > 3750.0:
+#             return redirect(url_for('vivir'))
+#         else:
+#             return redirect(url_for('morir'))
+#     else:
+#         return 'Para que quieres ir?'
+
+@app.route('/html')
+def inicio():
+    return '<html><body><h1>Xopa</h1></body></html>'
+
+@app.route('/')
+def indice():
+    return render_template('hola.html')
+
+@app.route('/usuario/<usuario>')
+def chotear_usuario(usuario):
+    return render_template('hola2.html', nombre=usuario)
 
 if __name__ == '__main__':
     app.run()
