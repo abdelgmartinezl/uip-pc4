96d76a85f371738b74703ad4094685449799da66
Abdel G. Martínez L <abdel.g.martinez.l@gmail.com>
2018-02-26 20:28:44 -0500

    Vamo pa Brazil

---
+++ 09.Flask/ejemplo/__init__.py
@@ -0,0 +1,21 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def choteo():
+    return 'Xopa moh!'
+
+@app.route('/saludar')
+def saludar():
+    return 'Saludos cordiales!'
+
+@app.route('/saludar/<nombre>')
+def saludar_nombre(nombre):
+    return 'Saludos, %s!' % nombre
+
+@app.route('/calculadora/<int:a>')
+def calcular_itbms(a):
+    return str(a*1.07)
+
+if __name__ == '__main__':
+    app.run()
